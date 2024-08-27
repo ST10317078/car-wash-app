c7bb101553c2b51bc02678c7da89e9db171d1e92
mpho molefe <131614864+ST10317078@users.noreply.github.com>
2024-08-27 15:31:17 +0200

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -43,7 +43,7 @@ const Signup = () => {
         <button type="submit">Sign Up</button>
         {error && <p className="error-message">{error}</p>} {/* Display error message */}
         <p>
-          Already have an account? <a href="/Login">Login</a>
+          Already have an account? <a href="/login">Login</a>
         </p>
       </form>
     </div>
